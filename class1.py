b3ce61fefb68f6ffa252d387e9826e2999360dae
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-05 22:55:48 +0530

    class1

---
+++ class1.py
@@ -0,0 +1,16 @@
+c=5
+d=7
+print(c, d) #output= 5 7
+
+c += 5
+d += 1
+print(c, d)  #output= 5 8
+
+e=5
+f=9
+print(e==f) # false
+print(e>f) #false
+print(not(e>f))#true
+print(e!=f) #true
+
+print(e==f and e<f) #false both must me correct for tru
