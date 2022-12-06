768fcd23a816c179924c05e13cd045db8e82579a
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-06 22:01:57 +0530

    Escape_seq

---
+++ Escape_seq.py
@@ -0,0 +1,8 @@
+# print("hello \"world\" world")
+# print('I\'m sarthak')
+
+
+print("first line\nsecond line \nthird line C")  #\n = to print next word in next line
+print("name\tsarthak_gagapalliwar")            #\t = for tap or space between two words
+print("for backslash \\")    #\\ = for printing \ and (for printing \\ take input = \\\\)
+print("sart\bhak")                #\b = use for backspace 
