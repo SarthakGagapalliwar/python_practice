a4506874a64621d8b29ca83fa4f17dca393d9374
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-07 19:43:50 +0530

    calculater

---
+++ Calculater.py
@@ -0,0 +1,27 @@
+print(3+5*4)
+print(2/4)  # /= floating point divison
+print(5//46) # integer division
+print(3**5) # ** = power like square(**=exponent)
+print(5**0.9) #**0.9 = square root
+print(round(5**0.7,6)) # round(  ,anynumber) = round of tile anynumber
+print(7%9)   #give remainder
+
+print((22+3)/52)
+
+
+print((5+6)*9/69) 
+
+
+# exponents  #go from right to left
+print(5**8**7)
+
+
+
+# + = addition 
+# - = subtraction
+# * = multiplication
+# / = float division 
+# // = integer Division
+# % = module, it give remainder
+# ** = exponent
+
