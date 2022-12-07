11666454b1ce0d92ee7d267ed59d6c5fc10b2ec5
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-07 19:30:42 +0530

    Emoji code

---
+++ Emoji.py
@@ -0,0 +1,7 @@
+# https://unicode.org/emoji/charts/full-emoji-list.html  = emoje link
+
+# print("U+1F602") conved U+1F600 =\U0001F602
+
+print("\U0001F602")
+print("\U0001F605")
+print("\U0001F601")
