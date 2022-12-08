a68e3679e9ec144f859220872cdf6f873b9183f6
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-08 20:27:33 +0530

    varible

---
+++ Varible_.py
@@ -0,0 +1,24 @@
+number1=2
+print(number1)
+number1=4
+print(number1)
+# string , number
+name = "sarthak"
+print(name)
+name=321
+print(name)
+
+#rule in variable
+# rule one
+#  1number = 4 error can start with number(1)
+
+#start with anu letter or underscrol (_)
+_name ="sarthak"
+
+# we cant use spacial symbol in varibale like $%&$ in first or in middle.
+# we can but number in middle but not in first in variable
+
+ #this first word first letter  we put SMALL and then other word we use first letter CAPS
+
+user_one_name = "sarthak" # sanke case writing putting(_)in words
+userOneName ="sarthak" # camel case writing in
