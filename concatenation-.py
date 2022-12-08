e7bb89dd9e7941ccc47966613d5b275745653022
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-08 20:25:50 +0530

    concatenation

---
+++ concatenation-.py
@@ -0,0 +1,10 @@
+first_name="ranu"
+last_name="papinwar"
+full_name = first_name +" "+ last_name
+print(full_name)
+
+# print(first_name + 3) #error can add with number
+print(first_name +"3") #no error
+print(first_name + str(3)) #no error  (string can added with string only)
+
+print(first_name * 3) # *3 we can do output will (sarthaksarthaksarthak)
