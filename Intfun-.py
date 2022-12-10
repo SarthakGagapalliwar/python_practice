03f2d1b1c4c4b74a480bda5893f59f859f5a98e5
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-10 22:21:46 +0530

    int_fun

---
+++ Intfun-.py
@@ -0,0 +1,31 @@
+num_1 = input("enter first number") #4
+numb_2 = input("enter second number") #4
+total = num_1 + numb_2
+print("total is " + total) #total = "4" + "4" = 44
+
+
+
+num_1 = int(input("enter first number")) #4
+num_2 = int(input("enter second number"))#4
+total = num_1 + num_2  #8
+# print("total is " + total) # error beacuse of total is no number
+print("total is " + str(total) )
+
+
+#str
+# 4 ---> "4"
+
+#int
+#"4"---> 4
+
+#float
+# "4" ---> 4.0
+
+
+number1 =str(4)
+number2 = float("44")
+number3 = int("33")
+print(number2 + number3) # final value in floot
+
+
+
