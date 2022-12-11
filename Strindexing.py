4574ee81399889ece417560b12872a3d5238ec1a
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-11 23:01:23 +0530

    Stringdexing

---
+++ Strindexing.py
@@ -0,0 +1,15 @@
+language = "string"
+# position(index number)
+
+#s=0 , -6
+#t=1 , -5
+#r=2 , -4
+#i=3 , -3
+#n=4 , -2
+#g=5 , -1
+
+print(language[4])
+#output=n
+print(language[-4])
+#output =r
+
