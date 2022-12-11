a21c27120c2fff77fa83cca01e64d9a81670b245
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-11 22:59:16 +0530

    strSlicing

---
+++ StrSlicing.py
@@ -0,0 +1,13 @@
+lang = "string"
+# print(lang[4])
+#syntax - [start argument : stop argument -1 ]
+print(lang[0:2]) 
+#otput = st
+print(lang[-3:6])
+#output= ing
+print(lang[:])
+#output = string
+print(lang[2:])
+#output = ring
+print(lang[:4])
+#output = stri
