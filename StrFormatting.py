dee029a7ee4b06e1f51cf1160008d5ed8898d628
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-12 22:23:05 +0530

    Strformatting

---
+++ StrFormatting.py
@@ -0,0 +1,9 @@
+name = "sujal"
+age = 24
+print("Hello " + name + " your age is " + str(age)) #ugly syntx
+
+# python 3
+print("Hello {} your ahe is {} ".format(name, age)) #nice syntx #python 3
+
+#python 3.6
+print(f"Hello {name} your age is {age}") #best syntax
