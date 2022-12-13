64b162d991935853b19de54369b0871ff3fd3801
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2022-12-13 21:42:03 +0530

    RmvSpace

---
+++ RmvSpaces.py
@@ -0,0 +1,8 @@
+name="   sujal   "
+dots="............"
+# lstrap() method
+print(name + dots)
+print(name.lstrip() + dots) #remove space from left
+print(name.rstrip() + dots) #remove space from right
+print(name.strip() + dots) #remove space from both side
+print(name.replace(" ","") + dots) #direct code to remove all space 
