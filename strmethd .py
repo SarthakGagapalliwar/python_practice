117021294df8d2a6ebd86d9eb24a226236de73a5
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-15 23:36:21 +0530

    Add files via upload

---
+++ strmethd .py
@@ -0,0 +1,24 @@
+name = "DhEeraJ MalI"
+
+#1. len() function
+print(len(name)) #output = 20
+
+
+#2. lower() method #in method we use (.)=dot
+print(name.lower())
+#output = sarthak gagapalliwar all in small character
+
+
+#3. upper() method
+print(name.upper())
+#output=SARTHAK GAGAPALLIWAR all character
+
+
+#4. title() method
+print(name.title())
+#output= Sarthak Gagapalliwar all word first letter name capital
+
+
+#5. count() method
+print(name.count("a")) #output = 5 
+#count the number how much in the word 
