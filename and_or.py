67c3969062c7e5e731c3bec618616399bffa501f
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-18 20:04:51 +0530

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,17 @@
+# check two condition at same time 
+# and , or
+
+#and
+name="cba"
+age=12
+if name=="cba" and age==19: #in and condition both much be true
+    print("conditon true")
+else:
+    print("condition false") 
+   
+   #or
+name="cba"
+if name=="abc" or age==12: #in or condition one then much be true
+    print("conditon true")
+else:
+    print("condition false")     
