e79c2b5b3e10984cdfe452c5461816d3d6449154
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-19 21:32:31 +0530

    Add files via upload

---
+++ heckEmpty.py
@@ -0,0 +1,8 @@
+# check empty or no
+# important
+name = input("enter your name : ")
+
+if name : # true if string is not empty
+    print("your name is ", name)
+else:
+    print("you did not type anything")    
