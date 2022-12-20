984529136d0b0563fcbd3e51d69b32179d88598a
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-20 23:05:28 +0530

    Add files via upload

---
+++ exercise33.py
@@ -0,0 +1,17 @@
+# EXERCISE ONE OF THREE
+# sun of n natural numbers
+# ask a user for natural number(N)
+# print total from 1 to n
+
+#ans = in 56lecture
+# ans
+n=int(input("enter a number"))
+total = 0
+i = 1
+while i <= n:
+    total += i  # total= total +i
+    i += 1
+print(total)
+#output = choise 10
+#1+2+3...10
+    
