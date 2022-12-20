49457f32e10facd6ee160a018fb9116a13c2e778
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-21 00:14:03 +0530

    Add files via upload

---
+++ loop_for_range.py
@@ -0,0 +1,12 @@
+# i = 0
+# while i <= 20:
+#     print("hello world")
+#     i += 1
+
+
+
+
+  # for loop with range function
+for i in range(1, 20): # 0 to 19
+    print("Hello world :", i)
+    print("This is line \n")    
