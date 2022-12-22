724521d6582f4b48b6aefd83120d178f5e4d4e6d
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-22 23:05:26 +0530

    Add files via upload

---
+++ for loop .py
@@ -0,0 +1,15 @@
+# for loop ex
+
+# sum from 1 to 10
+
+# total = 0
+# for i in range (1,11): #1+2+3......10
+#     total += i
+# print(total) output = 55
+
+
+n = int(input("Enter the number: "))
+total=0
+for i in range(1,n+1):
+    total += i
+print(total)
