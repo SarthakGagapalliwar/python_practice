9a9c13b605fd26b2e919180c7f563f91a362f1e0
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-25 20:46:27 +0530

    Add files via upload

---
+++ function intro.py
@@ -0,0 +1,20 @@
+#function
+
+
+
+# def add_two(a,b):
+#      return a+b
+# total = add_two(10,4)
+# print(total) # ==  print(add_two(10,4))
+
+def add_two(a,b):
+     return a+b
+# num1 =int(input("enter first number : ")) 
+# num2 =int(input("entr second number : "))
+# total = add_two(num1,num2)
+# print(total)
+
+
+first_name = input("Type first name : ")
+last_name = input("Type last name : ")
+print(add_two(first_name,last_name))
