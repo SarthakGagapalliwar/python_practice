70b68669eb3489ac79e9a3dddbf887726d8b669c
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-25 20:45:58 +0530

    Add files via upload

---
+++ for loop and string.py
@@ -0,0 +1,20 @@
+# name = "sarthak"
+# for i in range(len(name)):
+#     print(name[i])
+
+
+# name = "sarthak"
+# for i in name:
+#     print(i)
+
+# name = input("enter a name : ")
+# for i in name:
+#     print(i)
+
+# 1254----> 1+2+5+4
+num = input("Enter a number : ")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
+
