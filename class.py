6f874f03af4b542ebf78ed8846cae3a782dca4f9
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-28 21:46:42 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,15 @@
+# def greethello():
+#     print("hello,good afternoon")
+#     print("------------")
+# greethello()    
+
+
+
+def printTable(number):
+    i= 1
+    while i <11:
+        print(number*i)
+        i = i +1
+printTable(2)        
+print("__________")
+printTable(3)    
