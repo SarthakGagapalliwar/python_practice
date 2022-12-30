d322348b023a3415d13d203ed9bbcd9833aed24f
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-30 23:55:48 +0530

    Add files via upload

---
+++ Class19-summaryofchap1-CipherSchools.py
@@ -0,0 +1,40 @@
+# **********summary************
+
+# print function
+print("hello \"hello\'")
+
+# escape sequence
+print("line A \nline B")
+print("this is backlash \\")
+
+# escape sequence as normal text
+print(r"line A \n line B ")
+
+# python as calculater
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3) # float division 
+print(2//3) # integer division
+print(2**3**1) 
+#2**3 = (8) # right to left
+print(5*4/2) 
+#10 #left to right
+
+# variable
+name = "sarthak"
+print(name)
+name = 123
+print(name)
+
+# naming rules for variable
+# 1name = "sarthak" #error
+name1 = "sarthak"  # no error
+# name$ = "sarthak " # error
+# $name="sarthak" #error
+_name = "sartak" #no error
+
+# convention for variable
+
+user_one = "Sarthak" # sanke case writing
+userOne = "sarthak" # camel case writing 
